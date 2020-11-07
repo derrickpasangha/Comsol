d170b28608b1b06a6b4ae095ee3fc1c63aa6a5a0
Abhishek Sunkewar <abhisheksunkewar@icloud.com>
2020-11-07 12:11:22 +0530

    add announcements

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -37,6 +37,60 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget announcementCard(
+      String announcement, IconData icon, Color iconColor, String time) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.width * 0.025,
+        left: MediaQuery.of(context).size.width * 0.05,
+        right: MediaQuery.of(context).size.width * 0.05,
+        bottom: MediaQuery.of(context).size.width * 0.025,
+      ),
+      margin: EdgeInsets.only(
+        top: MediaQuery.of(context).size.width * 0.05,
+        left: MediaQuery.of(context).size.width * 0.05,
+        right: MediaQuery.of(context).size.width * 0.05,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.blue[400],
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                icon,
+                color: iconColor,
+              ),
+              Text(
+                time,
+                style: TextStyle(
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.015,
+              bottom: MediaQuery.of(context).size.height * 0.015,
+            ),
+            child: Text(
+              announcement,
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.028,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +100,28 @@ class _HomeState extends State<Home> {
           'Announcements',
         ),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          announcementCard(
+            'There will be a power cut in 4th Phase, Yelahanka New Town tomorrow from 9 A.M to 12 P.M. We regret the inconvenience caused.',
+            Icons.electrical_services,
+            Colors.amber,
+            '7th Nov, 12:03',
+          ),
+          announcementCard(
+            'Water will be supplied from 10 A.M to 3 P.M today throughout the city.',
+            Icons.waterfall_chart,
+            Colors.blue,
+            '6th Nov, 08:30',
+          ),
+          announcementCard(
+            'Garbage will be collected as usual today. Please keep your wet waste bins ready.',
+            Icons.delete,
+            Colors.grey,
+            '6th Nov, 08:30',
+          ),
+        ],
       ),
       drawer: Drawer(
         child: Column(
